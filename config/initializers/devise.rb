954eb2a5a05f8fe2130af512046e32390d361c67
Abhay Gupta <abhay.abhaygupta.gupta3@gmail.com>
2018-05-24 19:19:55 +0530

    Secret Key added

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -18,6 +18,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
+  config.secret_key = 'd9f10839841541db5b31049189f3851b2fb2e1db66586ccbd3290bd3e5f96c17f9a5413b8a3ae82e552c39c7eda9baecacc4d4133cf186c5405c0ac69338d729'
   config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
 
   # Configure the class responsible to send e-mails.
